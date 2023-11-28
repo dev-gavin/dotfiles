8c7263cbeae5046750ba5574cc784bca7732331e
Gavin Meeker <gmeeker99@gmail.com>
2023-11-27 20:54:11 -0500

    config

--- .config/nvim/lua/plugins/bufremove.lua
+++
@@ -1,8 +0,0 @@
-local M = {
-  -- "echasnovski/mini.bufremove",
-  -- keys = {
-  --   { "<leader>bd", false },
-  -- },
-}
-
-return M
--- .config/nvim/lua/plugins/vim-fugitive.lua
+++ .config/nvim/lua/plugins/vim-fugitive.lua
@@ -1,4 +1,5 @@
 local M = {
   "tpope/vim-fugitive",
+  lazy = false,
 }
 return M
